1b6fe6d8322e6b04fc803fe995489d2583bc46d7
morelllin <lenork890@gmail.com>
2025-06-16 12:11:00 +0600

    Add files via upload

---
+++ todo.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+root = tk.Tk() 
+root.geometry("700x400")
+root.configure(bg='#8A2BE2')
+root.title("ПАША ТЕХНИК УМЕР") 
+def delete_selected():
+    selected = listbox.curselection()
+    if selected:
+        listbox.delete(selected[0])
+def delete_all():
+        listbox.delete(0, tk.END)
+def add():
+    if textbox.get():
+        listbox.insert(0, textbox.get())
+
+label = tk.Label(root, text="100 дел которые надо сделать до конца жизни", font=("Arial", 18), bg="#D8BFD8") 
+label.pack(padx=20, pady=20,)
+textbox = tk.Entry(root, font=("Arial", 16), bg="#D8BFD8")
+textbox.pack()         
+select_button = tk.Button(root, text="Добавить", command=add, bg="#483D8B")
+select_button.pack()
+listbox = tk.Listbox(root, width=50, height=10, bg="#D8BFD8" )
+listbox.pack(pady=10)
+listbox.insert(tk.END, "встать с кровати")
+listbox.insert(tk.END, "почистить зубы")
+listbox.insert(tk.END, "выучить питон с нуля за 8 часов")
+listbox.insert(tk.END, "не покупать маленькой пиво")
+listbox.insert(tk.END, "cходить на любимую алгоритмизацию")
+delete_button = tk.Button(root, text="Удалить", command=delete_selected, bg="#483D8B")
+delete_button.pack()
+deleteall_button = tk.Button(root, text="Удалить всё", command=delete_all, bg="#483D8B")
+deleteall_button.pack()
+
+root.mainloop()
